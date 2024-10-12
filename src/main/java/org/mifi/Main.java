0f5a9c11e271fda7737d49ee6d6ea6586f6998f1
FiataliS <FiataliS@gmail.com>
2024-10-12 11:01:52 +0500

    refactor and adding parameters from user input

--- src/main/java/org/mifi/Main.java
+++ src/main/java/org/mifi/Main.java
@@ -5,23 +5,27 @@ import org.mifi.entyti.Forecasts;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final double LAT = 66.37125;
-        final double LON = 66.23388;
-        final int LIMIT = 7;  //Для тарифа «Тестовый» максимально допустимое значение — 7.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введете широту: ");
+        double lat = scanner.nextDouble();
+        System.out.print("Введете долготу: ");
+        double lon = scanner.nextDouble();
+        System.out.print("Введете колличество дней: ");
+        int limit = scanner.nextInt();  //Для тарифа «Тестовый» максимально допустимое значение — 7.
         int summ = 0;
         Gson gson = new Gson();
         Weather weather = new Weather();
-        String responseBody = weather.getResponseBodyFromLatAndLonByLimit(LAT, LON, LIMIT);
-        System.out.println("Response Body: " + responseBody);
+        String responseBody = weather.getResponseBodyFromLatAndLonByLimit(lat, lon, limit);
         Response response = gson.fromJson(responseBody, Response.class);
-        System.out.println("Температура fact{temp}: " + response.getFact().getTemp() + "°C");
+        System.out.println("Температура сегодня: " + response.getFact().getTemp() + "°C");
         for (Forecasts forecasts : response.getForecasts()) summ += forecasts.getTempAvgDay();
         String start = dateFormat(response.getForecasts().getFirst().getDate());
         String stop = dateFormat(response.getForecasts().getLast().getDate());
-        System.out.println("Средняя температура c " + start + " по " + stop + " равна: " + (LIMIT == 0 ? summ : summ / LIMIT) + "°C");
+        System.out.println("Средняя температура c " + start + " по " + stop + " равна: " + (limit == 0 ? summ : summ / limit) + "°C");
     }
 
     private static String dateFormat(Date date) {
